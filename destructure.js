4e4fde19ef7bd104f800e0e714c570f9048f4cd9
ravikumar <ravikumarwolfd9130@gmail.com>
2024-12-12 20:19:57 +0530

    destructure of object

---
+++ destructure.js
@@ -0,0 +1,10 @@
+const value={
+   name:"pradeep",
+   age:20,
+   number:1234567890,
+}
+const {name,age,number,mail="pradeep@gmail.com"}=value;
+console.log(name);
+console.log(age);
+console.log(number);
+console.log(mail);
