a9323b062be2989478599a08b22fc0225ed3da7d
pradeepgtech <pradeepgt29@gmail.com>
2024-12-17 18:33:10 +0530

    backticks to added

---
+++ backticks.js
@@ -0,0 +1,12 @@
+const Detail = {
+   name:"pradeep",
+   age:20,
+   phone:1234567890,
+   mail:"pradeepgt29@gmail.com"
+};
+
+console.log(` 
+ Name: ${Detail.name}
+ Age: ${Detail.age}
+ Phone ${Detail.phone}
+ Mail: ${Detail.mail}`);
