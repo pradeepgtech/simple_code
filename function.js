d4af18e4bd500b03d859ff492d4355e903a3425c
pradeepgtech <pradeepgt29@gmail.com>
2024-12-18 18:38:36 +0530

    this is a function coding

---
+++ function.js
@@ -0,0 +1,18 @@
+function addnum(num1,num2){
+   return(num1+num2);
+}
+let add=addnum(5,7);
+console.log(add);
+//output is: 12
+
+function say(){
+   console.log("hai pradeep");
+}
+say();
+//output is: hai pradeep
+
+function Name(name) {
+   return `Hello, ${name}`;
+}
+console.log(Name("pradeep G")); 
+// Output: Hello, pradeep G
