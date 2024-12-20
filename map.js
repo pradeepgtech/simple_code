7befdb2f90829c3eecb388bf29328aab16daa12e
pradeepgtech <pradeepgt29@gmail.com>
2024-12-20 18:51:35 +0530

    This is a map function coding

---
+++ map.js
@@ -0,0 +1,23 @@
+const num=[1,2,3,4]
+num.map((int)=>{
+   console.log(int)
+});
+//map function
+
+const value =[
+   {
+      name:"Pradeep",
+      age:20,
+   },
+   {
+      name:"Ragul",
+      age:21,
+   },
+   {
+      name:"Nitheesh",
+      age:22,
+   },
+];
+value.map((user)=>{
+   console.log(user.name)
+});
