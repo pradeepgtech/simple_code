fd57de544df396f4175440cbbe1f5cc1763cf4b4
pradeepgtech <pradeepgt29@gmail.com>
2024-12-23 19:23:33 +0530

    JavaScript object taken code

---
+++ object.js
@@ -0,0 +1,16 @@
+ class std {
+    constructor(name,age,number){
+       this.name=name;
+       this.age=age;
+       this.number=number;
+    }
+};
+student1=new std ("Pradeep",20,1234567890);
+student2=new std ("Nitheesh",19,9876543216);
+student3=new std ("Raghul",21,67892455638);
+
+console.log(student1.name);
+console.log(student2.age);
+console.log(student3.number);
+
+
